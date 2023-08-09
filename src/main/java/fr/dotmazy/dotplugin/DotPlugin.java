3696e3d36f8315b8ec3f8af08b2e4132e81e8cf1
Dotmazy <dotomazy@gmail.com>
2023-08-09 20:43:39 +0200

    Initial commit

---
+++ src/main/java/fr/dotmazy/dotplugin/DotPlugin.java
@@ -0,0 +1,17 @@
+package fr.dotmazy.dotplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DotPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

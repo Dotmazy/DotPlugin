b2c7bcb776aacb3b956128ff43b5d8fba75ae3f0
Dotmazy <95581415+Dotmazy@users.noreply.github.com>
2023-12-08 12:38:54 +0100

    Update Main.java

--- src/main/java/fr/dotmazy/example-extension/Main.java
+++ src/main/java/fr/dotmazy/example-extension/Main.java
@@ -1,1 +1,15 @@
+package fr.dotmazy.example_extension;
 
+import fr.dotmazy.example_extension.commands.*;
+import org.bukkit.Bukkit;
+import java.util.*;
+
+public class Main extends JavaPlugin {
+  
+    public void onEnable() {
+        System.out.println("Enabling Example DotExtension");
+
+        getServer().getPluginCommand("examplegui").setExecutor(new ExampleGuiCommand());
+    }
+
+}
